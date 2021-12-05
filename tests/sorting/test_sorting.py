5b187f02072d00857532727c8a98e30655bcc702
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2021-12-05 20:43:44 -0300

    ok

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,27 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs():
+    return [
+        {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
+        {"min_salary": 3000, "max_salary": 8000, "date_posted": "2020-01-01"},
+        {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
+        {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+        {"min_salary": 8000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    ]
+
+
+expected = [
+    {"min_salary": 3000, "max_salary": 8000, "date_posted": "2020-01-01"},
+    {"min_salary": 8000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
+    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
+]
+
+
+def test_sort_by_criteria(jobs):
+    final = sort_by(jobs, "max_salary")
+    assert final == expected
