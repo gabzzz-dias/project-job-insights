1832f0c8c8d09b2fcbb908fefa44e5bc91875666
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2021-12-05 19:37:37 -0300

    ok

--- src/insights.py
+++ src/insights.py
@@ -31,26 +31,18 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries = set()
+    jobs = read(path)
 
-    Must call `read`
+    for job in jobs:
+        if job["industry"] != "":
+            industries.add(job["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return list(industries)
 
 
 def filter_by_industry(jobs, industry):
     """Filters a list of jobs by industry
-
     Parameters
     ----------
     jobs : list
