ea1da06d8dff1224202e8a9d65027cdc871bdfd8
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2021-12-05 21:00:00 -0300

    ok

--- src/app.py
+++ src/app.py
@@ -7,3 +7,7 @@ def create_app() -> Flask:
     routes_and_views.init_app(app)
 
     return app
+
+# Agradecimentos ao Gabriel Miranda, da turma 10-B
+# que me ajudou a entender algumas questões do projeto em que tive dúvidas.
+# PR do Gabriel: github.com/tryber/sd-010-b-project-job-insights/pull/50
